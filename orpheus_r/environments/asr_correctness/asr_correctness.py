42fea52b276ac8395e66354d2db8d4146702d52e
Srinivas Billa <nbilla_expedia@github.com>
2025-09-04 17:59:38 +0100

    try except

--- orpheus_r/environments/asr_correctness/asr_correctness.py
+++ orpheus_r/environments/asr_correctness/asr_correctness.py
@@ -26,33 +26,36 @@ def load_environment(
         ds = load_dataset("srinivasbilla/orpheus-r-10k", split=split)
 
     def correct_answer(completion, answer):
-        headers = {
-            "Authorization": f"Bearer {api_token}"
-        }
-        data = {
-            "generated_text": completion[-1]['content']
-        }
-        decoded_audio_response = requests.post(audio_decoder_url, headers=headers, json=data)
-        base64_string = decoded_audio_response.json()['audio_array_base64']
-        audio_bytes = base64.b64decode(base64_string)
-        audio_array = np.frombuffer(audio_bytes, dtype=np.float32)
+        try:
+            headers = {
+                "Authorization": f"Bearer {api_token}"
+            }
+            data = {
+                "generated_text": completion[-1]['content']
+            }
+            decoded_audio_response = requests.post(audio_decoder_url, headers=headers, json=data)
+            base64_string = decoded_audio_response.json()['audio_array_base64']
+            audio_bytes = base64.b64decode(base64_string)
+            audio_array = np.frombuffer(audio_bytes, dtype=np.float32)
 
-        stt_client = OpenAI(base_url=stt_client_url, api_key=api_token)
+            stt_client = OpenAI(base_url=stt_client_url, api_key=api_token)
 
-        raw_audio = RawAudio.from_audio(Audio(
-                      audio_array=audio_array,
-                      sampling_rate=24000,
-                      format="wav",
-                  ))
-        
-        transcribed_text = stt_client.audio.transcriptions.create(
-            **TranscriptionRequest(
-                model="voxtral-speech-detection", audio=raw_audio, language="en", temperature=0.0
-            ).to_openai(exclude=("top_p", "seed"))
-        ).text
+            raw_audio = RawAudio.from_audio(Audio(
+                        audio_array=audio_array,
+                        sampling_rate=24000,
+                        format="wav",
+                    ))
+            
+            transcribed_text = stt_client.audio.transcriptions.create(
+                **TranscriptionRequest(
+                    model="voxtral-speech-detection", audio=raw_audio, language="en", temperature=0.0
+                ).to_openai(exclude=("top_p", "seed"))
+            ).text
 
-        difference = editdistance.eval(transcribed_text, answer)
-        return round(((len(answer) - difference)/len(answer))**2, 2)
+            difference = editdistance.eval(transcribed_text, answer)
+            return round(((len(answer) - difference)/len(answer))**2, 2)
+        except Exception as e:
+            return 0.0
     
     rubric = vf.Rubric(funcs=[correct_answer], weights=[1.0])
 
